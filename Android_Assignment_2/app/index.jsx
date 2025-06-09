509cba037083ed47731b3bdb43c3d11f824a453e
matthew.brett <matthewbrett61@gmail.com>
2025-06-09 11:15:37 -0230

    Movie info loading in console.

--- Android_Assignment_2/app/index.jsx
+++ Android_Assignment_2/app/index.jsx
@@ -1,4 +1,15 @@
 import { Text, View } from "react-native";
+import {useState} from "react";
+import Movie from "./components/Movie.jsx";
+
+const FotR = require("../assets/images/FotR.jpg");
+const TTT = require("../assets/images/TTT.jpg");
+const TRotK = require("../assets/images/TRotK.jpg");
+
+const images = {"FotR": FotR, "TTT": TTT, "TRotK": TRotK};
+
+import movieData from "../assets/movies.json";
+console.log(movieData);
 
 export default function Index() {
   return (
